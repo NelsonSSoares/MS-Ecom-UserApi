a6972613561dc0d31369796b63b129a516c45c4d
Nelson <punkpunkada@gmail.com>
2024-01-05 16:01:05 -0300

    adjunsted methods, returns address if user is active

--- src/main/java/nelsonssoares/ecomuserapi/usecases/usuario/ActiveUsuario.java
+++ src/main/java/nelsonssoares/ecomuserapi/usecases/usuario/ActiveUsuario.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import nelsonssoares.ecomuserapi.domain.entities.Usuario;
 import nelsonssoares.ecomuserapi.domain.entities.enums.PerguntaAtivo;
 import nelsonssoares.ecomuserapi.domain.repository.UsuarioRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -21,6 +23,8 @@ public class ActiveUsuario {
         Optional<Usuario> usuario = usuarioRepository.findById(id);
         if(usuario.isEmpty()){
             return null;
+        } else if (usuario.get().getAtivo().equals(PerguntaAtivo.SIM)) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Usuário já está ativo!");
         }
         Usuario user = usuario.get();
         user.setDataModificacao(LocalDate.now());
