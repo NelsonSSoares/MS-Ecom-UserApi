6f9cbc655733d543a28184c0e992ff24f75121b2
Nelson <punkpunkada@gmail.com>
2024-02-15 16:56:02 -0300

    preparando teste de repository

--- src/test/java/nelsonssoares/ecomuserapi/repositoty/TestUserRepository.java
+++ src/test/java/nelsonssoares/ecomuserapi/repositoty/TestUserRepository.java
@@ -1,4 +1,21 @@
 package nelsonssoares.ecomuserapi.repositoty;
 
+
+import nelsonssoares.ecomuserapi.domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@DataJpaTest
 public class TestUserRepository {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+
 }
